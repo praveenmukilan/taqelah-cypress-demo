47167817533a4e2ee0847455b0f2dddc77c8893b
praveen ms <praveenms@praveens-MacBook-Pro.local>
2019-03-26 23:08:01 +0800

    initial commit

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,16 @@
+///<reference types="Cypress" />
+
+describe('my first test', () => {
+
+    it('visit taqelah meetup', () => {
+        cy.visit('http://google.com')
+        cy.get('.gLFyf').type('taqelah')
+        cy.contains("Google Search").click()
+        cy.contains('Meetup').debug().click()
+        // cy.wait(3000)
+        // cy.contains('8th TAQELAH Meetup').click()
+        cy.get('.groupHomeHeader-groupNameLink').should('have.text',"TAQELAH")
+    })
+    
+    
+})
