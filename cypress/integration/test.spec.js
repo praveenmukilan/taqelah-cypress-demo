aa9e49780e647a81c83313accba57aec2b4c3fc8
praveen ms <praveenms@praveens-MacBook-Pro.local>
2019-03-27 00:25:28 +0800

    carousell site visit added

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -2,14 +2,23 @@
 
 describe('my first test', () => {
 
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
     it('visit taqelah meetup', () => {
-        cy.visit('http://google.com')
+
         cy.get('.gLFyf').type('taqelah')
         cy.contains("Google Search").click()
         cy.contains(/Meetup/).click()
-        // cy.wait(3000)
-        // cy.contains('8th TAQELAH Meetup').click()
         cy.get('.groupHomeHeader-groupNameLink').should('have.text',"TAQELAH")
+    })
+
+    it.only('visit carousell site', function() {
+        cy.get('.gLFyf').type('carousell')
+        cy.contains("Google Search").click()
+        cy.get('.LC20lb').first().should('contain','Snap to Sell').click()
+        cy.url().should('eq','https://sg.carousell.com/')
     })
     
     
